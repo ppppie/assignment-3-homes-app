a7fe65e6e70a3adc023c70db4299ecc633917ef9
Maddie Taghon <thessa.taghon@ndsu.edu>
2026-05-10 17:03:45 -0500

    completed creating housinglocation component and adding it to the angular app

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousingLocation],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ src/app/housing-location/housing-location.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-location.html',
+  styleUrl: './housing-location.css',
+})
+export class HousingLocation {}
